99e693ecc7adad3cb8a1beb21047d65cb9e3b0ee
brodrigues0ll <bernardorg99@hotmail.com>
2023-04-14 01:51:56 -0300

    Primeiros Inputs na tela, começando teste com mascara de dinheiro

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,42 @@
-import { Typography } from "@mui/material";
+import { InputAdornment, TextField, Typography } from "@mui/material";
+import { useState } from "react";
 
 export default function Home() {
+  const [quantity, setQuantity] = useState(10.95);
+  const [price, setPrice] = useState(30.82);
   return (
     <>
       <Typography variant="h3">Price Calculator - Primeira Parada</Typography>
+
       <Typography variant="h4"> Hello World!! </Typography>
+
+      <TextField
+        value={quantity}
+        type="number"
+        id="outlined-basic"
+        label="Quantidade"
+        variant="outlined"
+        InputProps={{
+          startAdornment: <InputAdornment position="start">UN</InputAdornment>,
+        }}
+        sx={{
+          width: "30%",
+        }}
+      />
+
+      <TextField
+        value={price}
+        type="number"
+        id="outlined-basic"
+        label="Preço Total"
+        variant="outlined"
+        InputProps={{
+          startAdornment: <InputAdornment position="start">R$</InputAdornment>,
+        }}
+        sx={{
+          width: "30%",
+        }}
+      />
     </>
   );
 }
