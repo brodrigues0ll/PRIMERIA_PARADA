98287b35d5eaed9994883c5cb51868529c0afe84
brodrigues0ll <bernardorg99@hotmail.com>
2023-04-20 23:24:56 -0300

    Sart Page Taxes header and buttons ok

---
+++ src/pages/taxes/index.jsx
@@ -0,0 +1,80 @@
+import { Box, Button, Typography } from "@mui/material";
+import Image from "next/image";
+import React from "react";
+
+const index = () => {
+  return (
+    <>
+      <Box
+        sx={{
+          color: "white",
+          fontFamily: "Sans-serif",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Image src="/assets/icons/backward.svg" width={40} height={40} />
+          <Typography
+            variant="h5"
+            sx={{
+              fontSize: "1.3rem",
+              width: "100%",
+              display: "flex",
+              justifyContent: "center",
+            }}
+          >
+            Calculadora de Taxas
+          </Typography>
+        </Box>
+
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <Button
+            variant="outlined"
+            sx={{
+              borderRadius: "3rem",
+              color: "white",
+              border: "1px solid red",
+              margin: "0 0.5rem",
+              marginTop: "3rem",
+              "&:hover": {
+                background: "red",
+                border: "1px solid red",
+              },
+            }}
+          >
+            Quero Cobrar
+          </Button>
+          <Button
+            variant="outlined"
+            sx={{
+              borderRadius: "3rem",
+              color: "white",
+              border: "1px solid red",
+              margin: "0 0.5rem",
+              marginTop: "3rem",
+              "&:hover": {
+                background: "red",
+                border: "1px solid red",
+              },
+            }}
+          >
+            Quero Receber
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default index;
