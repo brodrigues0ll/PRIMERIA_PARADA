95e256fc05853acf2eefa2de2b47db6b11d848e6
brodrigues0ll <bernardorg99@hotmail.com>
2023-05-08 01:59:23 -0300

    firebase configurado e funcionando login

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: "AIzaSyDnnLUt1yVS_M6btcaQiR5aQ2coKmRX-dg",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
