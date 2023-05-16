fa8c68ab12afef997d7dfbfdf9eee0d5bf334f33
brodrigues0ll <bernardorg99@hotmail.com>
2023-05-16 01:38:24 -0300

    Feat: Desfazendo configurações de cache

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,24 +3,8 @@ import "../../public/assets/fonts/Itim.css";
 import "../styles/main.css";
 import AuthRoute from "../components/AuthRoute";
 import Head from "next/head";
-import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      window.addEventListener("load", () => {
-        navigator.serviceWorker
-          .register("/service-worker.js")
-          .then((registration) => {
-            console.log("Service Worker registrado com sucesso:", registration);
-          })
-          .catch((error) => {
-            console.log("Falha ao registrar o Service Worker:", error);
-          });
-      });
-    }
-  }, []);
-
   return (
     <AuthRoute>
       <Head>
